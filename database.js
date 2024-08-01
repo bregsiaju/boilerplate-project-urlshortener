6654b4613ee2083d9c8a19b4501e9eead293b099
Bregsi Atingsari Julastri <20081010211@student.upnjatim.ac.id>
2024-08-01 09:27:44 +0000

    init challenge

---
+++ database.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        console.log(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
